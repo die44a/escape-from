d81b33c7c4bfc51a7275b3149acecda7f58410ff
dima borisov <dim4ig2007@gmail.com>
2026-04-28 20:05:16 +0500

    Напиши тесты, добавь конфиг, измени инсталлер

--- Assets/_Project/Runtime/Player/Installers/PlayerInstaller.cs
+++ Assets/_Project/Runtime/Player/Installers/PlayerInstaller.cs
@@ -1,3 +1,4 @@
+using _Project.Runtime.Core.Configs;
 using _Project.Runtime.Core.Main;
 using _Project.Runtime.Player.Controllers;
 using _Project.Runtime.Player.Main;
@@ -8,7 +9,11 @@ namespace _Project.Runtime.Player.Installers
 {
     public class PlayerInstaller : MonoInstaller
     {
-        [SerializeField] private GameObject playerPrefab; 
+        [Header("Player's prefabs")]
+        [SerializeField] private GameObject playerPrefab;
+        
+        [Header("Configurations")]
+        [SerializeField] private CoinsConfig coinsConfig;
         
         // ReSharper disable Unity.PerformanceAnalysis
         public override void InstallBindings()
@@ -34,7 +39,15 @@ namespace _Project.Runtime.Player.Installers
                 .AsSingle()
                 .NonLazy();
             
+            InstallConfigs();
+            
             Debug.Log("Player installed");
+        }
+
+        private void InstallConfigs()
+        {
+            Container.BindInstance(coinsConfig)
+                .AsSingle();
         }
     }
 }
