a434ad8738a972067050b089a0752d738dcb4eec
dima borisov <dim4ig2007@gmail.com>
2026-05-25 23:57:59 +0500

    Улучши ловушки

--- Assets/_Project/Runtime/Core/Traps/SpikeTrap.cs
+++ Assets/_Project/Runtime/Core/Traps/SpikeTrap.cs
@@ -1,11 +1,47 @@
+using _Project.Runtime.Core.General;
 using UnityEngine;
 
 namespace _Project.Runtime.Core.Traps
 {
     public class SpikeTrap : BaseTrap
     {
+        [SerializeField] private float knockbackForce = 2f;
+        [SerializeField] private float knockbackDuration = 0.15f;
+        [SerializeField] private bool applyKnockback = true;
+
         public void OnSpikesActionDamage()
-            => DealDamageToAll();
-            
+        {
+            DealDamageToAll();
+            ApplyKnockbackToAll();
+        }
+
+        private void ApplyKnockbackToAll()
+        {
+            if (!applyKnockback)
+                return;
+
+            if (TargetsInRange.Count == 0)
+                return;
+
+            var trapPosition = (Vector2)transform.position;
+
+            foreach (var target in TargetsInRange)
+            {
+                if (target == null)
+                    continue;
+
+                var component = target as Component;
+                if (!component)
+                    continue;
+
+                var controller = component.GetComponentInParent<MovementController>();
+                if (controller == null)
+                    continue;
+
+                var direction = ((Vector2)component.transform.position - trapPosition).normalized;
+
+                controller.ApplyKnockback(direction * knockbackForce, knockbackDuration);
+            }
+        }
     }
 }
